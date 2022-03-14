b43f5634e3584d7d0b171c8b0ca54828db044257
mcrussia228 <mcrussia223@gmail.com>
2022-03-15 00:50:18 +0500

    --v.beta--1:00.--15--

---
+++ js/join_as-FON.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#footer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 300.00,
+  minWidth: 300.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xe9fc00,
+  backgroundColor: 0x0,
+  maxDistance: 27.00,
+  spacing: 16.00
+})
