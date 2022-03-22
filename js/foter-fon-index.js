43dbb2b500f7de2059f507c7642f16dcab2c8834
mcrussia228 <mcrussia223@gmail.com>
2022-03-22 09:09:49 +0500

    --v.alpha--9:00.--22--

--- js/foter-fon-index.js
+++ js/foter-fon-index.js
@@ -3,7 +3,7 @@ VANTA.BIRDS({
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
+  minHeight: 410.00,
   minWidth: 1920.00,
   scale: 1.00,
   scaleMobile: 1.00,
